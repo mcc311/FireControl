9599c3429f27c94cf2352c42fda9315b0a639a86
XGungo <daniel2000890311g@gmail.com>
2022-04-15 17:57:59 +0800

    closing the marker whould save info into session

--- map.js
+++ map.js
@@ -16,46 +16,106 @@ const mb = L.tileLayer.mbTiles("./TaiwanEMap.mbtiles")
     })
     .addTo(map);
 
-markers_info = []
 
 function onMarkerClick(){
-    this.openPopup();
+    const id = this.options.id;
+    let $form = document.getElementById(`ship-form-${id}`);
+    for(let i = 0; i < $form.length; i++){
+        $form[i].value = window.sessionStorage.getItem($form[i].id);
+    }
 }
+
+let adding_enemy = true; // 預設為「新增敵人」
+function changeAddingMarker(){
+    adding_enemy = !adding_enemy;
+}
+
+class MarkerInfo {
+    constructor(id, is_enemy, lat, lng, ship_id=0, weapon1_id=0, weapon2_id=0) {
+        this.id = id;
+        this.is_enemy = is_enemy;
+        this.lat = lat;
+        this.lng = lng;
+        this.weapon1_id = weapon1_id;
+        this.weapon2_id = weapon2_id;
+
+    }
+}
+markers_info = {}
 
 function onMapClick(e) {
     let lat = e.latlng.lat; // 緯度
     let lng = e.latlng.lng; // 經度
+    const enemyIcon = L.icon({
+        iconUrl: "assets/img/map marker/blue pin.png",
+        iconSize: [30, 48],
+    })
+    const allyIcon = L.icon({
+        iconUrl: "assets/img/map marker/red pin.png",
+        iconSize: [30, 48],
+    })
 
-    marker = new L.marker(e.latlng, {draggable:'true', id: markers_info.length+1});
-    marker
+    let new_marker = new L.marker(e.latlng,
+        {
+            draggable: 'true',
+            id: Object.keys(markers_info).length + 1,
+            icon: adding_enemy ? enemyIcon : allyIcon,
+            is_enemy: adding_enemy
+        });
+    new_marker
         .on('dragend', function(event){
             const marker = event.target;
             const position = marker.getLatLng();
-            lat = position.lat;
-            lng = position.lng;
+            const lat = position.lat;
+            const lng = position.lng;
             console.log(position);
             marker
                 .setLatLng(position)
-                .setPopupContent(`緯度：${lat}<br/>經度：${lng}`)
                 .update();
         })
-        .bindPopup(`${shipForm(marker.options.id)}<br/>緯度：${lat}<br/>經度：${lng}`)
+        // .on('click', onMarkerClick)
+
+        .bindPopup(`${shipForm(new_marker)}<br/>緯度：${lat}<br/>經度：${lng}`)
         .on('click', onMarkerClick)
         .addTo(map);
-    markers_info.push(marker.options.id);
+    new_marker.openPopup();
+    new_marker
+        .getPopup().on('remove', ()=> {
+            const id = new_marker.options.id;
+            let $form = document.getElementById(`ship-form-${id}`);
+            for(let i = 0; i < $form.length; i++){
+                window.sessionStorage.setItem($form[i].id, $form[i].value);
+            }
+        })
+    markers_info[new_marker.options.id] = new MarkerInfo(new_marker.options.id, adding_enemy, lat, lng);
 }
 
-const shipForm = (id) =>{
-    let $form = $(`<form id="ship form-${id}"></form>`);
-    $form.append(toHTMLSelect(Enemy, "ship form-${id} enemy"));
-    $form.append(toHTMLSelect(EnemyWeapon, "ship form-${id} enemy-weapon"));
-    $form.append(toHTMLSelect(Ally, "ship form-${id} ally"));
-    $form.append(toHTMLSelect(AllyWeapon, "ship form-${id} ally-weapon"));
+
+const shipForm = (marker) =>{
+    const id = marker.options.id;
+    const is_enemy = marker.options.is_enemy;
+    let $form = $(`<form id="ship-form-${id}" class='form-check-inline'></form>`);
+    if(is_enemy){
+        $form.append(toHTMLSelect(Enemy, `ship-form-${id}_enemy`, '敵軍艦型'));
+        $form.append(toHTMLSelect(EnemyWeapon, `ship-form-${id}_enemy-weapon1`, '敵軍火力1'));
+        $form.append(toHTMLSelect(EnemyWeapon, `ship-form-${id}_enemy-weapon2`, '敵軍火力2'));
+    }else{
+        $form.append(toHTMLSelect(Ally, `ship-form-${id}_ally`, '我軍艦型'));
+        $form.append(toHTMLSelect(AllyWeapon, `ship-form-${id}_ally-weapon1`, '我軍火力1'));
+        $form.append(toHTMLSelect(AllyWeapon, `ship-form-${id}_ally-weapon2`, '我軍火力2'));
+    }
+    const pos = marker.getLatLng();
+    const lat = pos.lat;
+    const lng = pos.lng;
+
 
     return $('<div>').append($form).html();
 }
 
-const toHTMLSelect = (options, select_id)=>{
+const toHTMLSelect = (options, select_id, label)=>{
+    let $div = $('<div>');
+    $div.append($('<label>'), label);
+
     let $select = $(`<select id=${select_id}></select>`);
     options.forEach((opt)=>{
         $select.append($('<option>',{
@@ -63,7 +123,8 @@ const toHTMLSelect = (options, select_id)=>{
             text: opt.type
         }));
     });
-    return $select;
+    // return $select;
+    return $div.append($select);
 }
 
 const Enemy = [
