337df3a21c01aceb627062ee1283d6d04b726193
XGungo <daniel2000890311g@gmail.com>
2022-04-25 19:17:48 +0800

    add allowed_hosts

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nic5==tbd1j_oi%(na(uebz00%%fb+)iy)(t(j4az381n8gpk@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
