58e227390e32c424276986c9843a0b78e0e634b0
XGungo <daniel2000890311g@gmail.com>
2022-05-06 21:23:07 +0800

    修改 api doc 網址與 drf_yasg

--- backend/urls.py
+++ backend/urls.py
@@ -53,7 +53,7 @@ urlpatterns = [
     path('api/getPolicy', testPOST),
     path(r'map/', get_map),
     path(r'map/<int:bid>', get_result, name='get_result'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/doc', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
